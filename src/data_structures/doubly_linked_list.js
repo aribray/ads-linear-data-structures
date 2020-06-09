c50277d406c872266f4ae94c4e1e0fea012056a1
Ariana Bray <Ariana.Bray@nordstrom.com>
2020-06-09 13:23:23 -0700

    finally finishing

--- src/data_structures/doubly_linked_list.js
+++ src/data_structures/doubly_linked_list.js
@@ -31,24 +31,61 @@ class DoublyLinkedList {
   }
 
   insertHead(element) {
+    const newHead = new this.Node({
+      element: element, next: this._head(), prev: this._sentinel
+    });
+    this._head().prev = newHead;
+    this._sentinel.next = newHead;
+    if (this._sentinel.prev === this._sentinel) {
+      this._sentinel.prev = newHead;
+    }
+    return newHead;
   }
 
   insertTail(element) {
+    const newTail = new this.Node({
+      element: element, prev: this._tail(), next: this._sentinel
+    })
+    this._tail().next = newTail;
+    this._sentinel.prev = newTail;
+    if (this._sentinel.next === this._sentinel) {
+      this._sentinel.next = newTail;
+    }
+    return newTail;
   }
 
   removeHead() {
+    return this._head().remove();
   }
 
   removeTail() {
+    return this._tail().remove();
   }
 
   remove(node) {
+    if (node.remove) {
+      return node.remove();
+    }
   }
 
-  forEach(callback) {
+  forEach(callback, container = this) {
+    let currentNode = this._head();
+    let index = 0;
+    while (currentNode._active) {
+      callback(currentNode.element, index, container);
+      index += 1;
+      currentNode = currentNode.next;
+    }
   }
 
   count() {
+    let currentNode = this._head();
+    let elementCounter = 0;
+    while (currentNode._active) {
+      elementCounter += 1;
+      currentNode = currentNode.next;
+    }
+    return elementCounter;
   }
 }
 
